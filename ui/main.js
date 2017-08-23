0befb4fbb8398439933f5876f4552d2afd8ce493
Aditya Vallat <adivallat@gmail.com>
2017-08-23 19:47:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 
 var element=document.getElementById('maintext');
 element.innerHTML="bye value!";
+
+//move the image
+var element1=document.getElementById('img');
+element1.onclick=function()
+{
+    element1.style.marginLeft='100px';
+};
