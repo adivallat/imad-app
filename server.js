b0f8ea4b4dc31bed2bea2f195bbf1819b25e788d
Aditya Vallat <adivallat@gmail.com>
2017-08-23 21:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/pic1.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic1.jpg'));
 });
 
 
