566a4668e821fbab15d4c635a44ea2680fe06e3b
Aditya Vallat <adivallat@gmail.com>
2017-08-23 19:55:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,16 @@ element.innerHTML="bye value!";
 
 //move the image
 var element1=document.getElementById('img');
-element1.onclick=function()
-{
-    var interval=setInterval(moveright,100);
-    //element1.style.marginLeft='100px';
-};
+
 var marginleft=0;
 function moveright()
 {
     marginleft=marginleft+10;
     element1.style.marginleft=marginleft+'px';
 }
+
+element1.onclick=function()
+{
+    var interval=setInterval(moveright,100);
+};
+
